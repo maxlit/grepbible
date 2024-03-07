294d8956bcc1823dc3399cc8b82fe0d3ac489467
Maxim Litvak <maxim.litvak@gmail.com>
2024-03-07 18:28:35 +0100

    flag -d to download a bible

--- grepbible/cli.py
+++ grepbible/cli.py
@@ -1,10 +1,11 @@
 import argparse
-from grepbible.bible_manager import get_verse, list_bibles
+from grepbible.bible_manager import get_verse, list_bibles, download_and_extract_bible
 
 def main():
     parser = argparse.ArgumentParser(description="CLI tool to look up Bible verses.")
     parser.add_argument('-c', '--citation', help="Verse citation (e.g., 'Gen 1:1').", required=False)
     parser.add_argument('-v', '--version', help="Bible version(s), separated by commas.", required=False, default="kj")
+    parser.add_argument('-d', '--download', help="Download Bible version(s), separated by commas.", required=False)
     parser.add_argument('-l', '--list', action='store_true', help="List all available Bibles.")
     parser.add_argument('-i', '--interleave', action='store_true', help="Interleave verses for multiple versions.")
 
@@ -15,6 +16,9 @@ def main():
     elif args.citation:
         # Pass the interleave flag to the get_verse function
         get_verse(args.version, args.citation, args.interleave)
+    elif args.download:
+        for version in args.download.split(','):
+            download_and_extract_bible(version)
     else:
         parser.print_help()
 
